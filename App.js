d7aba00ffb4c7bb4e1ecc22f7d4dc52d1d459d13
Supriyo Mondal <hisupriyooo@gmail.com>
2020-04-04 21:19:44 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useState, Fragment } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default App = () => {
+  return (
+    <Fragment>
+      <View>
+        <Text>Hello from app</Text>
+      </View>
+    </Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+
+});
